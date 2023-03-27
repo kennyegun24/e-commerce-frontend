6c4a50e2f8cc935ae360a814806559c91958e726
Kenny Egun <kennyegun240@gmail.com>
2023-03-27 03:58:48 +0100

    Add routes and wrapped app around router-dom

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
 import './App.css';
+import Login from './components/auth/Login';
+import Register from './components/auth/Register';
+import { Routes, Route } from 'react-router-dom'
 
 const App = () => {
   return (
     <div className="app">
-
+      {/* <Login /> */}
+      <Routes>
+        <Route path='/register' element={<Register />} />
+        <Route path='/' element={<Login />} />
+      </Routes>
     </div>
   );
 }
