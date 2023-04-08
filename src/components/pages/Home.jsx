8283c342e734b886862fa50579482f31678a172f
Kenny Egun <kennyegun240@gmail.com>
2023-04-08 05:59:32 +0100

    Modify and allow add to cart for eachindividual item

--- src/components/pages/Home.jsx
+++ src/components/pages/Home.jsx
@@ -6,7 +6,9 @@ import { useDispatch, useSelector } from 'react-redux';
 import { getStores } from '../../redux/store/store';
 import { getAllProducts } from '../../redux/product/product';
 import { getCategory } from '../../redux/category/category';
-import { FaHourglass } from 'react-icons/fa'
+import { FaHourglass, FaPlus, FaMinus } from 'react-icons/fa'
+import { itemAdded } from '../../redux/cart/cart'
+import { v4 as uuid } from 'uuid'
 const Home = () => {
   const dispatch = useDispatch()
   const store = useSelector(state => state.store)
@@ -18,13 +20,18 @@ const Home = () => {
     dispatch(getStores())
     dispatch(getAllProducts())
   }, [])
-  console.log(products.length)
   const [currentPage, setCurrentPage] = useState(1)
   const [itemsPerPage, setItemsPerPage] = useState(15)
   const lastIndex = currentPage * itemsPerPage
   const firstIndex = lastIndex - itemsPerPage
   let currentItems = products.slice(firstIndex, lastIndex)
-
+  const [quantity, setNumOfOrder] = useState(1)
+  const increase = () => {
+    setNumOfOrder(quantity + 1)
+  }
+  const decrease = () => {
+    { quantity > 1 && setNumOfOrder(quantity - 1) }
+  }
   return (
     <div className='homeMainDiv'>
       {products.length < 1 ?
@@ -58,20 +65,41 @@ const Home = () => {
               <Row gutters={[32, 32]} className='rand'>
                 {
                   currentItems.map((prod) => {
+                    const state = prod
+
+                    const addItem = () => {
+                      dispatch(itemAdded({ id: uuid(), product: { state, price: prod.price * quantity }, quantity, price: prod.price * quantity }))
+                    }
                     return (
                       <Col xs={24} sm={12} lg={6} key={prod.id}>
-                        <NavLink state={prod} to={`/product/${prod.id}`} className='productsDiv' >
-                          <div className='prodLilImg'>
-                            <img className='prodImg' src={prod.image} alt="" />
+                        <div className='productsDivs'>
+                          <NavLink state={prod} to={`/product/${prod.id}`} className='productsDiv' >
+                            <div className='prodLilImg'>
+                              <img className='prodImg' src={prod.image} alt="" />
+                            </div>
+                            <div className='prodLilDetails'>
+                              <p className='bold'>{prod.category.name}</p>
+                              <p className='bold'>{prod.name}</p>
+                              <p className='bold red'>Price: ${prod.price}</p>
+                              <p className='bold greySlash'>Previous: ${prod.price * 1.2}</p>
+                              <p className="bold">{prod.store.store_name}</p>
+                            </div>
+                          </NavLink>
+
+                          <div className='addCartButton'>
+                            <div className='cartAddRedButtons'>
+                              <button className='addMinus'>
+                                <FaMinus className='' onClick={decrease} />
+                              </button>
+                              <span>{quantity}</span>
+                              <button className='addMinus'>
+                                <FaPlus className='' onClick={increase} />
+                              </button>
+                            </div>
+                            <button onClick={addItem} type="button" className='addCart'>Add to cart</button>
                           </div>
-                          <div className='prodLilDetails'>
-                            <p className='bold'>{prod.category.name}</p>
-                            <p className='bold'>{prod.name}</p>
-                            <p className='bold red'>Price: ${prod.price}</p>
-                            <p className='bold greySlash'>Previous: ${prod.price * 1.2}</p>
-                            <p className="bold">{prod.store.store_name}</p>
-                          </div>
-                        </NavLink>
+                        </div>
+
                       </Col>
                     )
                   })
@@ -93,7 +121,6 @@ const Home = () => {
             <div className='home'>
               {
                 store.stores.map((sto) => {
-                  console.log(sto)
                   return (
                     <NavLink key={sto.id} state={sto.product} to={`/store/${sto.id}/products`} className='homeCatDv'>
                       <img className='catImg' src={sto.image} alt="" />
