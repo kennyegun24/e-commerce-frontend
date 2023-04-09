30546398665449883975de0471a9142b8cc4a5b3
Kenny Egun <kennyegun240@gmail.com>
2023-04-09 22:51:01 +0100

    min and max values price add to redux state

--- src/redux/search.js
+++ src/redux/search.js
@@ -4,14 +4,21 @@ const searchSlice = createSlice({
     name: 'search',
     initialState: {
         search: '',
+        minVal: 0,
+        maxVal: 0,
     },
     reducers: {
         searchInp: (state, action) => {
             state.search = action.payload
-            // console.log(action.payload)
+        },
+        getMinVal: (state, action) => {
+            state.minVal = action.payload
+        },
+        getMaxVal: (state, action) => {
+            state.maxVal = action.payload
         }
     }
 })
 
-export const { searchInp } = searchSlice.actions;
+export const { searchInp, getMinVal, getMaxVal } = searchSlice.actions;
 export default searchSlice.reducer;
