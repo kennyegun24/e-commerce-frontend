ec8b4a8b2fa1816add2e3bdda28305c9166d4931
Kenny Egun <kennyegun240@gmail.com>
2023-04-10 17:37:00 +0100

    Modified and add icons on each text field

--- src/components/auth/Login.jsx
+++ src/components/auth/Login.jsx
@@ -4,6 +4,8 @@ import { Link } from 'react-router-dom'
 import { useDispatch } from 'react-redux'
 import { login } from '../../redux/apiCalls'
 import { loginFailure } from '../../redux/user/user'
+import { FaEnvelope, FaLock, FaRegEnvelope } from 'react-icons/fa'
+import './login.css'
 const Login = () => {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
@@ -37,15 +39,23 @@ const Login = () => {
 
   return (
     <FormDiv>
-      <Form>
+      <form className='loginForm'>
         <H1>Login to your account</H1>
-        <Input type='email' onChange={(e) => setEmail(e.target.value)} placeholder='Enter email' />
-        <Input type='password' onChange={(e) => setPassword(e.target.value)} placeholder='Enter password' />
-        <FormBtn type='submit' onClick={loginUser}>Login</FormBtn>
+        <div className='emailDiv'>
+          <input className='loginInput' type='email' onChange={(e) => setEmail(e.target.value)} placeholder='Enter email' />
+          <FaRegEnvelope className='mailLock' />
+        </div>
+
+        <div className='emailDiv'>
+          <input className='loginInput' type='password' onChange={(e) => setPassword(e.target.value)} placeholder='Enter password' />
+          <FaLock className='mailLock' />
+        </div>
+
+        <button className='loginBtn' type='submit' onClick={loginUser}>Login</button>
         {err && setTimeout(() => { setErr(null) }, 5000) && <span style={{ color: 'red', fontWeight: '600' }}>{err}</span>}
         {success && setTimeout(() => { setSuccess(null) }, 5000) && <span style={{ color: 'green', fontWeight: '600' }}>{success}</span>}
-        <P>Don't have an account? <Link to='register'>Register...</Link></P>
-      </Form>
+        <P>Don't have an account? <Link to='/register'>Register...</Link></P>
+      </form>
     </FormDiv>
   )
 }
