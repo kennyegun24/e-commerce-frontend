b9cdfbd45df699fc47c75fc7706cdf9444a14059
Kenny Egun <kennyegun240@gmail.com>
2023-04-11 10:40:27 +0100

    Persisted store and cart

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,18 +1,46 @@
 import { configureStore } from '@reduxjs/toolkit'
-// import userSlice from '../user/user'
-import userSlice from './user/user'
-import productSlice from './product/product'
-import cartSlice from './cart/cart'
-import storeSlice from './store/store'
-import categorySlice from './category/category'
-import searchSlice from './search'
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER
+} from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+import userSlice from './user/user';
+import productSlice from './product/product';
+import cartSlice from './cart/cart';
+import storeSlice from './store/store';
+import categorySlice from './category/category';
+import searchSlice from './search';
+
+const persistConfig = {
+  key: 'root',
+  version: 1,
+  storage,
+}
+
+const persistedReducer = persistReducer(persistConfig, userSlice)
+const persistedCartReducer = persistReducer(persistConfig, cartSlice)
+// const persistedReducer = persistReducer(persistConfig, userCartReducer)
+
 export const store = configureStore({
   reducer: {
-    user: userSlice,
+    user: persistedReducer,
+    cart: persistedCartReducer,
     product: productSlice,
-    cart: cartSlice,
     store: storeSlice,
     category: categorySlice,
     search: searchSlice
-  }
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 })
+export const persistor = persistStore(store)
