dd2a932218daa54d81e7fed37b44aea8884fbd31
Kenny Egun <kennyegun240@gmail.com>
2023-04-27 06:21:19 +0100

    Modify to get 3 category products from backend

--- src/redux/category/category.js
+++ src/redux/category/category.js
@@ -12,12 +12,20 @@ export const getCategory = createAsyncThunk('category/category/1', async () => {
   return data
 })
 
+export const getOneCategory = createAsyncThunk('category/category/1/prod', async (id) => {
+  const res = await fetch(`http://localhost:3000/api/v1/categories/${id}/products`)
+  const data = await res.json()
+  console.log(data)
+  return data
+})
+
 const categorySlice = createSlice({
   name: 'category',
   initialState: {
     categories: [],
     category: [],
-    status: null
+    oneCategory: [],
+    status: false
   },
   reducers: {},
   extraReducers: (reduce) => {
@@ -33,8 +41,19 @@ const categorySlice = createSlice({
         const isFulfilled = state;
         isFulfilled.status = 'Fulfilled'
         const dataJson = action.payload.data
-        const toJSON = JSON.parse(dataJson)
-        isFulfilled.category = toJSON
+        // const toJSON = JSON.parse(dataJson)
+        isFulfilled.category = dataJson
+      })
+      .addCase(getOneCategory.pending, (state, action) => {
+        const isFulfilled = state;
+        isFulfilled.status = true
+      })
+      .addCase(getOneCategory.fulfilled, (state, action) => {
+        const isFulfilled = state;
+        isFulfilled.status = false
+        // isFulfilled.status = 'Fulfilled'
+        const dataJson = action.payload.data
+        isFulfilled.oneCategory = dataJson
       })
   },
 })
