b926abdcf6cec7496a5340fcfdee58a36af77a07
Kenny Egun <kennyegun240@gmail.com>
2023-04-27 06:21:50 +0100

    Modify to get three stores products from backend

--- src/redux/store/store.js
+++ src/redux/store/store.js
@@ -1,9 +1,10 @@
 import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-  status: null,
+  status: false,
   stores: [],
-  allStores: []
+  allStores: [],
+  oneStore: []
 }
 export const getStores = createAsyncThunk('store/store', async () => {
   const res = await fetch('http://localhost:3000/api/v1/stores')
@@ -14,6 +15,13 @@ export const getStores = createAsyncThunk('store/store', async () => {
 export const getAllStores = createAsyncThunk('store/allStore', async () => {
   const res = await fetch('http://localhost:3000/api/v1/stores/all')
   const data = await res.json()
+  return data
+})
+
+export const getOneStore = createAsyncThunk('store/Store/:id', async (id) => {
+  const res = await fetch(`http://localhost:3000/api/v1/stores/${id}/products`)
+  const data = await res.json()
+  console.log(data)
   return data
 })
 
@@ -34,6 +42,20 @@ const storeSlice = createSlice({
         // const dataJson = action.payload.data
         // const toJSON = JSON.parse(dataJson)
         isFulfilled.allStores = action.payload.data
+      })
+      .addCase(getOneStore.pending, (state) => {
+        const isFulfilled = state;
+        isFulfilled.status = true;
+        // const dataJson = action.payload.data
+        // const toJSON = JSON.parse(dataJson)
+        // isFulfilled.oneStore = action.payload.data
+      })
+      .addCase(getOneStore.fulfilled, (state, action) => {
+        const isFulfilled = state;
+        isFulfilled.status = false;
+        // const dataJson = action.payload.data
+        // const toJSON = JSON.parse(dataJson)
+        isFulfilled.oneStore = action.payload.data
       })
   }
 })
