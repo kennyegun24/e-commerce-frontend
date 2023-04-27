54ae79bb7968284a4d6c17307a99c21b8c2d599e
Kenny Egun <kennyegun240@gmail.com>
2023-04-27 16:34:10 +0100

    Major update, display message after remove item from cart

--- src/components/pages/Cart.jsx
+++ src/components/pages/Cart.jsx
@@ -1,85 +1,94 @@
-import React from 'react'
-import { FaTrash } from 'react-icons/fa'
+import React, { useState } from 'react'
+import { FaInfoCircle, FaTrash } from 'react-icons/fa'
 import { useDispatch, useSelector } from 'react-redux'
-import { NavLink } from 'react-router-dom'
-import { deleteCart, itemAdded, loadCart } from '../../redux/cart/cart'
+import { NavLink, Link } from 'react-router-dom'
+import { deleteCart } from '../../redux/cart/cart'
 import './cart.css'
+import Success from './Success'
 
 const Cart = () => {
   const product = useSelector(state => state.cart)
+  const [success, setSuccess] = useState(false)
   const dispatch = useDispatch()
   const deleteCartItem = (id, price) => {
     dispatch(deleteCart({ id, price }))
+    setSuccess(true)
   }
   return (
     <div className='cart'>
       <div className="cartDetailDiv">
-        <p style={{ textAlign: 'center', fontSize: '2rem' }}>Cart</p>
-        <div className='cartDisplaySmDv'>
-
-          {product.products.length < 1 ? <p className='center'>Cart is empty</p> :
-            (product.products.map((cart) => {
-              return (
-                <div className='cartDisplay' key={cart.id} >
-                  <NavLink state={cart.product.state} to={`/product/${cart.product.state.id}`} className='flexCart'>
-                    <img src={cart.product.state.image} style={{ borderRadius: '12px', height: '75px', width: '75px' }} alt="" />
-                    <div>
-                      <h3>
-                        {cart.product.state.name}
-                      </h3>
-                      <p>
-                        {cart.product.state.name}
-                      </p>
-                      <p>
-                        {cart.product.state.color}
-                      </p>
-                    </div>
-                  </NavLink>
-
-                  {/* <div>
-                    <p>
-                      {cart.quantity}
-                    </p>
-                    <p>
-                      {cart.product.state.color}
-                    </p>
-                  </div> */}
-
-                  <div>
-                    <p>
-                      ${cart.price}
-                    </p>
-                    <p>
-                      {cart.quantity}
-                    </p>
-                  </div>
-
-                  <FaTrash onClick={() => deleteCartItem(cart.id, cart.product.state.price * cart.quantity)} />
+        {product.products.length > 0 && <p style={{ height: '5vh', textAlign: 'center', fontSize: '2rem' }}>Cart</p>}
+        {
+          success && (setTimeout(() => { setSuccess(false) }, 4000)) && (
+            <Success text={'Removed from cart'} color={'#111'} success={success} />
+          )}
+        {
+          product.products.length < 1 ?
+            (
+              <div className='center'>
+                <p style={{ opacity: '0.5', display: 'flex', alignItems: 'center', gap: '0.2rem' }}>Cart is empty <FaInfoCircle /></p>
+                <div style={{ display: 'flex', gap: '0.5rem', alignItems: 'center' }}>
+                  <p>Go back to</p><Link className='link' to='/'>homepage</Link>
                 </div>
-              )
-            }))
-          }
-        </div>
-        <div className='checkout'>
-          <h2 style={{ textAlign: 'center' }}>Payment Details</h2>
-          <div>
-            <div>
-              <p>Items:</p>
-              <p>${product.products.length > 0 ? Math.ceil(product.total) : 0}</p>
-            </div>
+              </div>
+            )
+            :
+            (
+              <>
+                <div className='cartDisplaySmDv'>
+                  {product.products.map((cart) => {
+                    return (
+                      <div className='cartDisplay' key={cart.id} >
 
-            <div>
-              <p>Shipping(incl. taxes):</p>
-              <p>${product.products.length > 0 ? 20 : 0}</p>
-            </div>
+                        <NavLink state={cart.product} to={`/product/${cart.product.id}`} className='flexCart'>
+                          <img src={cart.product.image} style={{ borderRadius: '12px', height: '75px', width: '75px' }} alt="" />
+                          <div>
+                            <h3 className='cartProdName'>
+                              {cart.product.name}
+                            </h3>
+                            <p>
+                              {cart.product.color}
+                            </p>
+                          </div>
+                        </NavLink>
 
-            <div>
-              <p>Total: </p>
-              <p>${Math.ceil(product.total) + (product.products.length > 0 ? 20 : 0)}</p>
-            </div>
-          </div>
-          <button className='checkoutBtn'>Checkout</button>
-        </div>
+                        <div>
+                          <p>
+                            ${cart.price}
+                          </p>
+                          <p>
+                            Qty: {cart.quantity}
+                          </p>
+                        </div>
+
+                        <FaTrash onClick={() => deleteCartItem(cart.id, cart.product.price * cart.quantity)} />
+                      </div>
+                    )
+                  })}
+                </div>
+
+                <div className='checkout'>
+                  <h2 style={{ textAlign: 'center' }}>Payment Details</h2>
+                  <div>
+                    <div>
+                      <p>Items:</p>
+                      <p>${product.products.length > 0 ? Math.ceil(product.total) : 0}</p>
+                    </div>
+
+                    <div>
+                      <p>Shipping(incl. taxes):</p>
+                      <p>${product.products.length > 0 ? 20 : 0}</p>
+                    </div>
+
+                    <div>
+                      <p>Total: </p>
+                      <p>${Math.ceil(product.total) + (product.products.length > 0 ? 20 : 0)}</p>
+                    </div>
+                  </div>
+                  <button className='checkoutBtn'>Checkout</button>
+                </div>
+              </>
+            )}
       </div>
     </div>
   )
